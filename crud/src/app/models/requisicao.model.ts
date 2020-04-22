48dfaee923a8d279992933d6dde49447cdebe091
Diego Souza <diegosouza@bestpontos.com.br>
2020-04-21 22:16:06 -0300

    Adicionando propriedades da classe requisicao

--- crud/src/app/models/requisicao.model.ts
+++ crud/src/app/models/requisicao.model.ts
@@ -1,2 +1,14 @@
-export class Requisicao {
+import { Funcionario } from './funcionario.model';
+import { Departamento } from './departamento.model';
+import { Movimentacao } from './movimentacao.model';
+import { Model } from '../core/model';
+
+export class Requisicao extends Model {
+    solicitante: Funcionario;
+    dataAbertura: any;
+    ultimaAtualizacao: any;
+    descricao: string;
+    status: string;
+    destino: Departamento;
+    movimentacoes: Movimentacao[];
 }
