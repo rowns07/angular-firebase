8f9ee076e2fba7e27d49e87014df6d06dc00b681
Diego Souza <diegosouza@bestpontos.com.br>
2020-04-23 23:56:07 -0300

    Criando AuthGuard para validar rota conforme login

---
+++ crud/src/app/services/authguard.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { take, map, tap } from 'rxjs/operators';
+import Swal from 'sweetalert2'
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthguardService implements CanActivate {
+
+  constructor(private afAuth: AngularFireAuth, private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    return this.afAuth.authState.pipe(
+      take(1),
+      map(user => !!user),
+      tap(loggedIn => {
+        if (!loggedIn) {
+          this.router.navigate(['/login']);
+          Swal.fire('Esta logado?','Não identificamos seu login')
+        }
+      })
+    )
+  }
+}
